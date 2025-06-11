f13afed2f159704e9d1fac33c6bcffbd5dcc9b85
alialsaffarcodexals <justforcodexals@gmail.com>
2025-06-12 00:29:03 +0300

    Add Go server and Dockerfile

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./"))
+	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.ServeFile(w, r, "login.html")
+			return
+		}
+		fs.ServeHTTP(w, r)
+	}))
+
+	log.Println("Serving on :1234")
+	if err := http.ListenAndServe(":1234", mux); err != nil {
+		log.Fatal(err)
+	}
+}

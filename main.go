30401590c5207fc463535f63421372f7b38932fd
alialsaffarcodexals <justforcodexals@gmail.com>
2025-06-12 01:23:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./"))
+
 	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
 			http.ServeFile(w, r, "login.html")
@@ -16,8 +17,8 @@ func main() {
 		fs.ServeHTTP(w, r)
 	}))
 
-	log.Println("Serving on :1234")
-	if err := http.ListenAndServe(":1234", mux); err != nil {
+	log.Println("Serving on 0.0.0.0:1234")
+	if err := http.ListenAndServe("0.0.0.0:1234", mux); err != nil {
 		log.Fatal(err)
 	}
 }
